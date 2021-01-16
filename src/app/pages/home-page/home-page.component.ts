24be48317832a28a0aec3a0ba4800e088ef8ba78
vahdian <germanpalero@gmail.com>
2021-01-16 02:19:43 +0100

    basic structure

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  popoverIsVisible = false;
   constructor() { }
 
   ngOnInit(): void {
+   
+  }
+  showPopover(){
+    console.log(this.popoverIsVisible);
+    this.popoverIsVisible = true
+  }
+  hidePopover(){
+    console.log(this.popoverIsVisible);
+    this.popoverIsVisible = false
   }
 
 }
